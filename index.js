741177240fd1f3c05970a2adc15f96a3269eb532
ken4ro <kenken5232@gmail.com>
2019-01-27 22:14:05 +0900

    bugfix

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+'use strict';
+
 // もろもろの準備
-var video = document.getElementById("video");           // video 要素を取得
+var video = document.getElementById("my-video");           // video 要素を取得
 var canvas = document.getElementById("canvas");         // canvas 要素の取得
 var context = canvas.getContext("2d");                  // canvas の context の取得
 
@@ -11,12 +13,91 @@ var media = navigator.mediaDevices.getUserMedia({       // メディアデバイ
 });
 media.then((stream) => {                                // メディアデバイスが取得できたら
     video.srcObject = stream; // video 要素にストリームを渡す
-    $('#my-video').get(0).srcObject = stream;
     localStream = stream;
 }).catch(function (error) {
     // Error
     console.error('getUserMedia() error: ', error);
 });
+
+// Peer オブジェクト作成
+var peer = new Peer({
+    key: '22bed81b-406a-4fec-a08c-998f8cd7588d',
+    debug: 3
+});
+
+// SkyWay のシグナリングサーバと接続完了
+peer.on('open', function () {
+    $('#my-peer-id').text(peer.id);
+    console.log("peer open. Connection to SkyWay is succeeded");
+})
+
+// 部屋作成ボタンクリック
+$('create-room').submit(function (e) {
+    // e.preventDefault();
+    var formText = document.getElementById("my-peer-id-text").formText;
+    console.log("my peer id form text: ", formText);
+});
+
+// 入室ボタンクリック
+$('enter-room').submit(function (e) {
+    // e.preventDefault();
+    // 指定した Peer ID と接続
+    var formText = document.getElementById("her-peer-id-text").formText;
+    console.log("her peer id form text: ", formText);
+    var herPeerId = formText;
+    callToTargetPeer(peer, herPeerId);
+    // 現在入室中の部屋のIDを表示
+    document.getElementById("now-id").formText = herPeerId;
+});
+
+// 相手から接続要求があった
+peer.on('call', function (call) {
+    // 接続要求に応答
+    call.answer(localStream);
+    // call オブジェクト更新
+    setupCallEventHandlers(call);
+});
+
+// エラー発生
+peer.on('error', function (err) {
+    alert(err.message);
+});
+
+// シグナリングサーバーとの接続が切断された
+peer.on('disconnected', function () {
+    console.log("Connection to SkyWay is disconnected");
+});
+
+// 対象の Peer ID に発信
+function callToTargetPeer(peer, targetPeerId) {
+    // 発信
+    const call = peer.call(targetPeerId, localStream);
+    // call オブジェクト更新
+    setupCallEventHandlers(call);
+}
+
+// 発信 or 着信
+let existingCall = null;
+function setupCallEventHandlers(call) {
+    // 既存の接続を切断
+    if (existingCall) {
+        existingCall.close();
+    };
+    existingCall = call;
+    // 相手のカメラ映像・マイク音声を受信
+    call.on('stream', function (stream) {
+        $('#her-video').get(0).srcObject = stream;
+        $('#make-call').hide();
+        $('#end-call').show();
+        $('#her-peer-id').text(call.remoteId);
+    });
+    // 相手との接続が切断
+    call.on('close', function () {
+        $('#' + call.remoteId).remove();
+        $('#make-call').show();
+        $('#end-call').hide();
+    });
+}
 
 // clmtrackr の開始
 var tracker = new clm.tracker();  // tracker オブジェクトを作成
@@ -44,84 +125,3 @@ function showData(pos) {
     var dat = document.getElementById("dat");             // データ表示用div要素の取得
     dat.innerHTML = str;                                  // データ文字列の表示
 }
-
-// Peer オブジェクト作成
-let existingCall = null;
-peer = new Peer({
-    key: 'apikey',
-    debug: 3
-});
-
-// 発信処理
-let targetPeerId = 1;
-const call = peer.call($('#callto-id').val(), localStream);
-setupCallEventHandlers(call);
-
-// 切断処理
-existingCall.close();
-
-// 着信処理
-peer.on('call', function(call){
-    call.answer(localStream);
-    setupCallEventHandlers(call);
-});
-
-// Callオブジェクト必要なイベントリスナー
-function setupCallEventHandlers(call){
-    if (existingCall) {
-        existingCall.close();
-    };
-
-    existingCall = call;
-
-    call.on('stream', function(stream){
-        addVideo(call, stream);
-        setupEndCallUI();
-        $('#their-id').text(call.remoteId);
-    });
-    call.on('close', function(){
-        removeVideo(call.remoteId);
-        setupMakeCallUI();
-    });
-}
-
-function addVideo(call, stream){
-    $('#their-video').get(0).srcObject = stream;
-}
-
-function removeVideo(peerId){
-    $('#'+peerId).remove();
-}
-
-function setupMakeCallUI(){
-    $('#make-call').show();
-    $('#end-call').hide();
-}
-
-function setupEndCallUI(){
-    $('#make-call').hide();
-    $('#end-call').show();
-}
-
-// 各種コールバック
-peer.on('open', function(){
-    // SkyWay のシグナリングサーバと接続完了
-    $('#my-id').text(peer.id);
-    var dat = document.getElementById("dat");
-    dat.innerText = "Connection to SkyWay is succeeded";
-})
-
-peer.on('error', function(err){
-    alert(err.message);
-});
-
-peer.on('close', function(){
-    // Peer (相手) との接続が切断された
-    console.log("Peer close");
-});
-
-peer.on('disconnected', function(){
-    // シグナリングサーバーとの接続が切断された
-    console.log("Connection to SkyWay is disconnected");
-});
-
